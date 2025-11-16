f1dc3da4737887e0aa2418c487cb5a130df76ce6
Vijay Pratap Singh <123111274+pratap-vj@users.noreply.github.com>
2025-11-17 00:04:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+import pickle
+import pandas as pd
+from data_model import Water
+
+
+app = FastAPI(
+    title="Water Potability Prediction ",
+    description="Predicting water potability using machine learning",
+)
+
+with open("D:\Project\WaterPortabilityPrediction\model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+
+@app.get("/")
+def index():
+    return {"message": "Welcome to the Water Potability Prediction API"}
+
+@app.post("/predict")
+def model_predict(water: Water):
+    sample = pd.DataFrame([water.dict().values()], columns=water.dict().keys()) 
+    predicted = model.predict(sample)
+    
+
+    if predicted[0] == 1:
+        return "The water is potable"
+    else:
+        return "The water is not potable"
+    
+
+
+
